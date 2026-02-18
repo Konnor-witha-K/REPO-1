c94427138920c434b1538ae8c807a431df141f98
Konnor-witha-K <s1049471@student.knoxschools.org>
2026-02-18 15:52:02 +0000

    created function and tested it with original parameters

--- coding_challenge.py
+++ coding_challenge.py
@@ -1,7 +1,14 @@
 # The Series 1^1 + 2^2 + 3^3... 10^10 = 10405071317
 # Find the last ten digits of the series up to 1000^1000
 
-def count_sequence():
-    print("Hello")
 
-count_sequence()
+def count_sequence():
+    sum = 0
+    x = 1
+    for i in range(1, 11):
+        y = x**x
+        sum = sum + y
+        x += 1
+    return sum
+
+print(f"The Series of equal powers up to 10 == {count_sequence()}")
